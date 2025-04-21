6afe976676584a5c79b36e64d13e6fe7f6fbd427
Daniel Sánchez Betancor <daniel.sb.contractor@bbva.com>
2025-04-21 14:51:26 +0200

    @1.0.1 @Add ticket created template and refactor template handling

--- helpers/templatesHelper.ts
+++ helpers/templatesHelper.ts
@@ -1,6 +1,21 @@
 enum TEMPLATES {
   PORTFOLIO,
+  TICKET_CREATED,
 }
+
+const getTicketCreatedTemplate = () => {
+  return (ticketId: number, pass?: string) => {
+    return `Hello!
+    Your ticket has been created successfully.
+
+    You can refer to the ticket ID "${ticketId}" for any further queries.
+    ${pass ? `Your pass to access the ticket is: ${pass}` : ""}
+
+    Thank you for using our service!`;
+  }
+}
+
+
 const getTemplate = (
   template: TEMPLATES,
   from: string,
@@ -10,7 +25,20 @@ const getTemplate = (
   switch (template) {
     case TEMPLATES.PORTFOLIO:
       return `From ${name} - ${from} \n\n ${message}`;
+    default:
+      return `From ${name} - ${from} \n\n ${message}`;
   }
 };
 
-export { TEMPLATES, getTemplate };
+const buildTemplate = (
+  template: TEMPLATES,
+) => {
+  switch (template) {
+    case TEMPLATES.TICKET_CREATED:
+      return getTicketCreatedTemplate();
+    default:
+      return getTicketCreatedTemplate();
+  }
+}
+
+export { TEMPLATES, getTemplate, buildTemplate };
